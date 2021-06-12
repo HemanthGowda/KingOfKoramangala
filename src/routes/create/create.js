75473fd713c64623d32a5c5da535667425942d81
Derick Mathew <derick.mathew@everest.engineering>
2021-06-12 11:31:24 +0530

    base ui for the create page

--- src/routes/create/create.js
+++ src/routes/create/create.js
@@ -1,3 +1,40 @@
+import {Button, Col, Container, FormControl, InputGroup, Row} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
+
+
 export function Create() {
-	return <h2>Create</h2>;
+	const history = useHistory();
+	let userHandle, gameCode;
+
+	function CreateGame() {
+		history.push("/game/" + gameCode);
+	}
+
+	return <Container fluid>
+		<Row className="justify-content-md-center">
+			<Col sm={5}>
+				<Row>
+					<FormControl
+						placeholder="User Handle"
+						aria-label="User Handle"
+						onChange={event => {
+							userHandle = event.target.value
+						}}
+					/>
+				</Row>
+				<Row>
+					<FormControl
+						placeholder="Game Code"
+						aria-label="Game Code"
+						onChange={event => {
+							gameCode = event.target.value
+						}}
+					/>
+				</Row>
+				<Row>
+						<Button margin="auto" variant="outline-primary" onClick={CreateGame}>Create</Button>
+				</Row>
+			</Col>
+		</Row>
+	</Container>;
 }
