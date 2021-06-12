d983f8316a4f16cf23be41bd6830c938adafa2c1
HemanthGowda <hemanthfeb25@gmail.com>
2021-06-12 10:36:22 +0530

    Added home page

--- src/App.js
+++ src/App.js
@@ -10,13 +10,20 @@ function App() {
 				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
 				<Switch>
-					<Route path="/">
-						<Home/>
-					</Route>
 					<Route path="/create">
 					</Route>
 					<Route path="/join">
 						<Join/>
+					</Route>
+
+					{/* If none of the previous routes render anything,
+                        this route acts as a fallback.
+
+                        Important: A route with path="/" will *always* match
+                        the URL because all URLs begin with a /. So that's
+                        why we put this one last of all */}
+					<Route path="/">
+						<Home/>
 					</Route>
 				</Switch>
 			</div>
--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,3 +1,11 @@
+import {Col, Container, Nav, Row} from "react-bootstrap";
+
 export function Home() {
-	return <h2>Home</h2>;
+	return <Container fluid>
+		<Row className="justify-content-md-center">
+			<Nav.Link href="/create">Create Game</Nav.Link>
+			<Col md="auto"/>
+			<Nav.Link href="/join">Join Game</Nav.Link>
+		</Row>
+	</Container>
 }
