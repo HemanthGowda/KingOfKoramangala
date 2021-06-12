bba647bc5bdabb5afae33385a0b4333bc85ef481
HemanthGowda <hemanthfeb25@gmail.com>
2021-06-12 10:05:24 +0530

    Import missing components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Link} from "react-router-dom";
+import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
 
 function App() {
 	return (
