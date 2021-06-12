41e9bcd26581f8136b0f2543899cccd5acd6c4eb
Derick Mathew <derick.mathew@everest.engineering>
2021-06-12 10:40:27 +0530

    adding simple create page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Join} from "./routes/join/join";
 import {Home} from "./routes/home/home";
+import {Create} from "./routes/create/create";
 
 function App() {
 	return (
@@ -10,13 +11,14 @@ function App() {
 				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
 				<Switch>
-					<Route path="/">
-						<Home/>
-					</Route>
 					<Route path="/create">
+						<Create />
 					</Route>
 					<Route path="/join">
 						<Join/>
+					</Route>
+					<Route path="/">
+						<Home/>
 					</Route>
 				</Switch>
 			</div>
