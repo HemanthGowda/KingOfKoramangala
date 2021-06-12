e69fdf6ed4b6fe132de01529fb342b5621d36dcb
HemanthGowda <hemanthfeb25@gmail.com>
2021-06-12 11:54:02 +0530

    Game screen skeleton

--- src/routes/game/game.js
+++ src/routes/game/game.js
@@ -1,3 +1,22 @@
+import {Col, Container, Row} from "react-bootstrap";
+import "./game.css"
+
 export function Game() {
-	return <h1>Board Goes Here</h1>
+	return <Container>
+		<Row>
+			<Col xs={2} className={"player-layout"}>
+				<Row className={"vertical-player"}/>
+				<Row className={"vertical-player"}/>
+			</Col>
+			<Col xs={8} className={"board-layout"}>
+				<Row className={"horizontal-player"}/>
+				<Row className={"board"}/>
+				<Row className={"horizontal-player"}/>
+			</Col>
+			<Col xs={2} className={"player-layout"}>
+				<Row className={"vertical-player"}/>
+				<Row className={"vertical-player"}/>
+			</Col>
+		</Row>
+	</Container>
 }
